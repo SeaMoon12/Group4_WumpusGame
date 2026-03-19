76990a2fe99d719f837192e1ce7a863f7c995627
S3AM00N <130579522+SeaMoon12@users.noreply.github.com>
2026-03-19 15:56:36 +0700

    feature: added declarations in header files

--- HuntTheWumpus/WumpusGame/include/Hazard.h
+++ HuntTheWumpus/WumpusGame/include/Hazard.h
@@ -5,6 +5,26 @@
 
 class Hazard
 {
+public:
+	Hazard();
+	virtual void encounter() = 0;
+
+	// GETTERS
+	int getRoomID();
+	static int getActiveHazardCount();
+	virtual string getHazardName() = 0;
+	virtual string getHazardWarning() = 0;
+
+	// SETTERS
+	void setRoomID();
+
+	virtual ~Hazard();
+
+protected:
+	int roomID;
+
+private:
+	static int activeHazardCount;
 };
 
 #endif
--- HuntTheWumpus/WumpusGame/include/SuperBat.h
+++ HuntTheWumpus/WumpusGame/include/SuperBat.h
@@ -2,9 +2,23 @@
 #define SUPER_BAT_H
 
 #include "libraries.h"
+#include "Hazard.h"
 
-class SuperBat
+class SuperBat : public Hazard
 {
+public:
+	SuperBat(float movementChance);
+	void encounter() override;
+
+	// GETTERS
+	string getHazardName() override;
+	string getHazardWarning() override;
+
+	void roam();
+
+	~SuperBat() override;
+private:
+	float chanceToMove;
 };
 
 #endif
